8e4472e81ceba97b6032fa2c7a6e666bca0d70df
Kaitlin Ryan <kaitlinaryan1@gmail.com>
2018-02-22 16:34:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+const driver = {};
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+return Object.assign({}, driver, { [key]: value });
+}
+function destructivelyUpdateDriverWithKeyAndValue(){
+  driver.name = 'Sam';
+  driver.address = '12 Broadway';
+  return driver
+}
+
+function deleteFromDriverByKey(driver, key) {
+  delete driver.name;
+  delete driver.address;
+  return driver;
+}
+
+function destructivelyDeleteFromdriverByKey(driver, key) {
+return driver;
+}
